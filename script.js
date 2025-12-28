3e7ca82af10aec6f477a6f6decd4ebb772bc9c04
Zebony919 <zebony9@gmail.com>
2025-12-28 15:31:28 +1300

    Write logic for playing one round

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello, World!")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const choice = Math.random() * 100;
+
+    if (choice <= 32) {
+        return "rock"
+    } else if (choice <= 65) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Please enter you move (Rock, Paper, Scissors): ");
+    return choice
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if ((humanChoice == "rock" && computerChoice == "scissors") || 
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")) {
+            humanScore++;
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+        } else if ((humanChoice == "rock" && computerChoice == "paper") || 
+        (humanChoice == "paper" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "rock")) {
+            computerScore++;
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        } else {
+            console.log("Tie");
+        }
+}
+
+
+
