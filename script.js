ba80ec501b9a547d93899d70daefc68f9acafbd4
Zebony919 <zebony9@gmail.com>
2025-12-28 15:37:33 +1300

    Write logic for playing the full five rounds and showing who won at the end

--- script.js
+++ script.js
@@ -36,5 +36,21 @@ function playRound(humanChoice, computerChoice) {
         }
 }
 
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice()
+        let computerChoice = getComputerChoice()
+        playRound(humanChoice, computerChoice)
+    }
 
+    if (humanScore > computerScore) {
+        console.log("Player Wins!")
+    } else if (computerScore > humanScore) {
+        console.log("Computer Wins!")
+    } else {
+        console.log("Tie!")
+    }
+}
+
+playGame()
 
